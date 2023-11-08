acf462e48dc04c37edf90406fbaa89bb77cb7296
Piyush <piyushabhiranjan30@gmail.com>
2023-11-09 00:16:01 +0530

    React Essential

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
-import { CORE_CONCEPTS } from './data.js';
+import CoreConcepts from './components/CoreConcepts.jsx';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
-import { EXAMPLES } from './data.js';
+import Examples from './components/Examples.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
+  // const [selectedTopic, setSelectedTopic] = useState();
 
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-    // console.log(selectedTopic);
-  }
+  // function handleSelect(selectedButton) {
+  //   // selectedButton => 'components', 'jsx', 'props', 'state'
+  //   setSelectedTopic(selectedButton);
+  //   // console.log(selectedTopic);
+  // }
 
-  console.log('APP COMPONENT EXECUTING');
+  // console.log('APP COMPONENT EXECUTING');
 
-  let tabContent = <p>Please select a topic.</p>;
+  // let tabContent = <p>Please select a topic.</p>;
 
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
+  // if (selectedTopic) {
+  //   tabContent = (
+  //     <div id="tab-content">
+  //       <h3>{EXAMPLES[selectedTopic].title}</h3>
+  //       <p>{EXAMPLES[selectedTopic].description}</p>
+  //       <pre>
+  //         <code>{EXAMPLES[selectedTopic].code}</code>
+  //       </pre>
+  //     </div>
+  //   );
+  // }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
+        <CoreConcepts/>
+        <Examples/>
+        {/* <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
             {CORE_CONCEPTS.map((conceptItem)=><CoreConcept key={conceptItem.title} {...conceptItem}/>)}
@@ -46,10 +46,10 @@ function App() {
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+            <CoreConcept {...CORE_CONCEPTS[3]} /> 
           </ul>
-        </section>
-        <section id="examples">
+        </section> */}
+        {/* <section id="examples">
           <h2>Examples</h2>
           <menu>
             <TabButton isSelected={selectedTopic==='components'} onSelect={() => handleSelect('components')}>
@@ -60,9 +60,9 @@ function App() {
             <TabButton isSelected={selectedTopic==='state'} onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
           {tabContent}
-        </section>
+        </section> */}
       </main>
-    </div>
+    </>
   );
 }
 
